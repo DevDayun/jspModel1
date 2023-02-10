d6923e0171802187e3f8443ca0da19b7a670538b
Dev_Dayun <devdayun11@gmail.com>
2023-02-10 10:37:01 +0900

    Update DBConnection.java

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -21,7 +21,7 @@ public class DBConnection {
 		Connection conn = null;
 		try {
 			// 데이터베이스에 연결하기 위한 정보(연결 URL, 데이터베이스 ID, 데이터베이스 PW)
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "jenolove9500!");
+			conn = DriverManager.getConnection();
 			System.out.println("Connection Success");
 		} catch (SQLException e) {
 			System.out.println("db를 연결하지 못했습니다");
